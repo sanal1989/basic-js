5a75e93720259e9e447458c27ab071854bd1eff5
sanal1989 <55034623+sanal1989@users.noreply.github.com>
2020-02-27 12:58:36 +0300

    add: complete task dram-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,21 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let result = [];
+  if( members == null){
+    return false;
+  }else if(members =="undefined"){
+    return false;
+  }
+  for(let i=0;i<members.length;i++){
+    if(typeof members[i] == "string"){
+    	if(members[i].trim().search("^[A-Za-z]")>-1){
+      	result.push(members[i].trim().toUpperCase().charAt(0))
+    	}
+  	}
+  }
+  if(result.length>0){
+    return result.sort().join("");
+  }else{
+    return false;
+  }
+  
 };
